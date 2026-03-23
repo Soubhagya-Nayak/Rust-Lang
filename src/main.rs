203c06ae9041539ac8c88bf0820aa670c3626b77
Soubhagya Nayak <155530626+Soubhagya-Nayak@users.noreply.github.com>
2026-03-23 20:19:23 +0530

    about datatypes and scopes.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-fn main() {
+// ---------------------------- Variables --------------------------------- //
+/*fn main() {
     // snake_case: hello_world - rust follow
     // camelCase: helloWorld - js follow
 
@@ -17,5 +18,60 @@ fn main() {
     let emp_info: (&str, u8) = ("Soubhagya", 21);
     let emp_name = emp_info.0;
     let emp_age = emp_info.1;
+
+    // Destructuring
+    let (employe_name, employe_age) = emp_info;
+
     println!("Employe Name: {}, Employe Age: {}", emp_name, emp_age);
+    println!(
+        "Employee Name: {}, Employee Age: {}",
+        employe_name, employe_age
+    );
+} */
+
+// ---------------------------- Function --------------------------------- //
+
+/*
+fn main() {
+    print_name();
+    print_numeric(56);
+
+    let num1: u8 = 10;
+    let num2: u8 = 20;
+    let result: u8 = add(num1, num2);
+    println!("Result: {}", result);
 }
+
+fn print_name() {
+    println!("Soubhagya Nayak");
+}
+
+fn print_numeric(item: u8) {
+    println!("Number is: {}", item);
+}
+
+fn add(num1: u8, num2: u8) -> u8 {
+    return num1 + num2;
+}
+*/
+
+// ---------------------------- Scope --------------------------------- //
+
+/*
+fn main() {
+    let outer_variable: u8 = 23;
+    // Outer Variable
+    {
+        let inner_varibale: u8 = 34;
+        // Inner Variable
+        println!("Inner Variable: {}", inner_varibale);
+        println!("Outer Varibale: {}", outer_variable);
+    }
+
+    // println!("Inner Variable: {}", inner_varibale); // Can't use inner_variable beacuse of scope validation.
+    println!("Outer Varibale: {}", outer_variable);
+}
+*/
+
+// ---------------------------- Ownership --------------------------------- //
+fn main() {}
